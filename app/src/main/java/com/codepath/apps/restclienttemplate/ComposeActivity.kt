4916f0e5fb530e0856bbacb29549271d12e2ad9b
Massimo Chen <haomassimochen@gmail.com>
2022-10-06 21:26:15 -0400

    First Try character limit

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -1,16 +1,22 @@
 package com.codepath.apps.restclienttemplate
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.codepath.apps.restclienttemplate.models.Tweet
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 
+const val CHARACTER_LIMIT = 280
 class ComposeActivity : AppCompatActivity() {
     lateinit var etCompose:EditText
     lateinit var btnTweet:Button
@@ -35,14 +41,12 @@ class ComposeActivity : AppCompatActivity() {
             }
 //            2.Make sure tweet is under character count
             else{
-                if (tweetContent.length>140){
-                    Toast.makeText(this,"Text is too long", Toast.LENGTH_SHORT).show()
+                if (tweetContent.length> CHARACTER_LIMIT){
+                    Toast.makeText(this,"Text is too long. Limit is ${CHARACTER_LIMIT.toString()} characters", Toast.LENGTH_SHORT).show()
                 }
                 else{
                     client.publishTweet(tweetContent, object : JsonHttpResponseHandler() {
                         override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
-
-                            Log.i(TAG, "Tweet successfully published!")
                             val tweet = Tweet.fromJson(json.jsonObject)
 
                             val intent = Intent()
@@ -59,15 +63,33 @@ class ComposeActivity : AppCompatActivity() {
                         ) {
                             Log.i(TAG, "Failure tweeting!", throwable)
                         }
-
-
-
                     })
                 }
             }
-
         }
+            val tvCount = findViewById<TextView>(R.id.tvCount)
+            etCompose.addTextChangedListener(object : TextWatcher {
+                val tweetContent = this.toString()
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                    //reset tvCount text color in any case beforehand to prevent problems
+                    tvCount.setTextColor(Color.GRAY)
+                }
+                @SuppressLint("SetTextI18n")
+                override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                    if (s != null) {
+                        if(s.length > CHARACTER_LIMIT) {
+                            tvCount.setTextColor(Color.RED)
+                        }
+                        tvCount.text = "Characters left: ${CHARACTER_LIMIT - s.length}"
+                    }
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+                    TODO("Not yet implemented")
+                }
+            })
     }
+
     companion object {
         val TAG = "ComposeActivity"
     }
